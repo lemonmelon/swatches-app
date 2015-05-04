2dab0901d06eb704427d374fa5bb25746d7eac3a
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-05-04 17:58:47 +0200

    remove newline

--- app/src/main/java/dk/lemonmelon/swatches/LoadSwatches.java
+++ app/src/main/java/dk/lemonmelon/swatches/LoadSwatches.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 
 
 public class LoadSwatches extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
