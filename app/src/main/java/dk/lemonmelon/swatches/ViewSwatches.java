a72c8bcfb7c8928d824566ed080701fe3ee8a1a2
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-05-04 11:03:58 +0200

    Removed code for settings menu

--- app/src/main/java/dk/lemonmelon/swatches/ViewSwatches.java
+++ app/src/main/java/dk/lemonmelon/swatches/ViewSwatches.java
@@ -104,26 +104,4 @@ public class ViewSwatches extends ActionBarActivity {
 
         return v;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_view_swatches, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
