c5fb9b5cf89b33ddad45465a46fa797781354712
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-05-04 10:47:03 +0200

    Added LoadSwatches activity (loading screen code)

---
+++ app/src/main/java/dk/lemonmelon/swatches/LoadSwatches.java
@@ -0,0 +1,16 @@
+package dk.lemonmelon.swatches;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LoadSwatches extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_swatches);
+    }
+}
